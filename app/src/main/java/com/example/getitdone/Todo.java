bc43caa1a821fd569a595cd8fbe6546f0d47df6c
Chan Tun Aung <u6777573@anu.edu.au>
2019-10-14 17:44:39 +1100

    some minor changes

--- app/src/main/java/com/example/getitdone/Todo.java
+++ app/src/main/java/com/example/getitdone/Todo.java
@@ -107,6 +107,6 @@ public class Todo implements Serializable {
 
     @Override
     public String toString() {
-        return this.todoName + " " + this.completed;
+        return this.todoName;
     }
 }
