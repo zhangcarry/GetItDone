744a644e6a6bacce07e91e0e3078416f122d8dd4
Carry Zhang <u6499267@anu.edu.au>
2019-10-15 14:54:27 +1100

    Notification with button

--- app/src/main/java/com/example/getitdone/MainActivity.java
+++ app/src/main/java/com/example/getitdone/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -191,7 +192,7 @@ public class MainActivity extends AppCompatActivity
 
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Reminder", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Reminder", NotificationManager.IMPORTANCE_HIGH);
             channel.setDescription("Reminder");
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
@@ -199,14 +200,21 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void sendNotification() {
+        Intent notificationIntent = new Intent(this,MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,0,notificationIntent,0);
+        Intent broadcastIntent = new Intent(this, NotificationListener.class);
+        PendingIntent actionIntent = PendingIntent.getBroadcast(this,0,broadcastIntent,PendingIntent.FLAG_CANCEL_CURRENT);
 
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
         Notification notification = new NotificationCompat.Builder(this,CHANNEL_ID)
                 .setCategory(NotificationCompat.CATEGORY_REMINDER)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setSmallIcon(R.drawable.ic_check_box)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setSmallIcon(R.drawable.ic_check)
+                .setAutoCancel(true)
+                .addAction(R.drawable.ic_check,"Complete",actionIntent)
                 .setContentTitle("Reminder")
                 .setContentText("Test message")
+                .setContentIntent(contentIntent)
                 .build();
 
         notificationManagerCompat.notify(1,notification);
