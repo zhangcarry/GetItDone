6c4045b66672d068204616857c20cb908e6c7ea0
saomao <saomao@saomaos-MacBook-Pro.local>
2019-10-18 17:25:07 +1100

    aa

--- app/src/main/java/com/example/getitdone/myAlarm.java
+++ app/src/main/java/com/example/getitdone/myAlarm.java
@@ -33,8 +33,6 @@ public class myAlarm extends BroadcastReceiver {
     public void sendNotification(Context context, String title) {
         Intent notificationIntent = new Intent(context,MainActivity.class);
         PendingIntent contentIntent = PendingIntent.getActivity(context,0,notificationIntent,0);
-        Intent broadcastIntent = new Intent(context, NotificationListener.class);
-        PendingIntent actionIntent = PendingIntent.getBroadcast(context,0,broadcastIntent,PendingIntent.FLAG_CANCEL_CURRENT);
 
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
         Notification notification = new NotificationCompat.Builder(context,CHANNEL_ID)
@@ -42,9 +40,8 @@ public class myAlarm extends BroadcastReceiver {
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setSmallIcon(R.drawable.ic_bell_check)
                 .setAutoCancel(true)
-                .addAction(R.drawable.ic_check,"Complete",actionIntent)
                 .setContentTitle(title)
-                .setContentText(title + " is due today")
+                .setContentText(title + " is due now")
                 .setContentIntent(contentIntent)
                 .setColor(Color.rgb(37,5,109))
                 .build();
